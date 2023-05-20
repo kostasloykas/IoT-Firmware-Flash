78d6972941e824f254cbb4a67b5ab37170f09f7e
Kostas Loukas <kostas99loukas@gmail.com>
2023-05-20 14:16:40 +0300

    update app

--- flash_app/src/App.svelte
+++ flash_app/src/App.svelte
@@ -2,22 +2,21 @@
 	import * as script from "../scripts/script";
 </script>
 
-<head>
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-	/>
-	<script
-		src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-	></script>
-</head>
-
 <main>
+	<div>
+		<link
+			rel="stylesheet"
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+		/>
+		<script
+			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+		></script>
+	</div>
 	<div class="d-flex justify-content-center m-5">
 		<div
 			class=" col-sm-5 text-center border border-success border-3 rounded-3 p-3"
 		>
-			<label class="h2 fw-bold">Upload file</label>
+			<span class="h2 fw-bold">Upload file</span>
 			<input
 				type="file"
 				id="image"
