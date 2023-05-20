04bf317e5a42402f8de9326f85089fdcf5f96de2
Kostas Loukas <kostas99loukas@gmail.com>
2023-05-20 16:51:27 +0300

    update app

--- flash_app/src/App.svelte
+++ flash_app/src/App.svelte
@@ -4,10 +4,6 @@
 
 <main>
 	<div>
-		<script
-			src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"
-		></script>
-
 		<link
 			rel="stylesheet"
 			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
