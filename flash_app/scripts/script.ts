d44041b3d6c6e9f8889ce1d1462347a80fafaab8
Kostas Loukas <kostas99loukas@gmail.com>
2023-05-21 17:37:37 +0300

    update app

--- flash_app/scripts/script.ts
+++ flash_app/scripts/script.ts
@@ -7,21 +7,24 @@ import { GetTypeOfDevice } from './library';
 
 let device_selected :boolean = false;
 let image_selected :boolean = false;
+let timeout:any = null;
 
 
 
 // ====================== FUNCTIONS ==================
 
 function Alert(message:string,type_of_alert:string, duration:number = 4000) {
-    let timeout:number;
-    clearTimeout(timeout);
-
+    if (timeout !== null) {
+        clearTimeout(timeout);
+    }
+    
     $("#message_div").removeClass(["alert-success","alert-danger"]);
     $("#message_div").addClass("alert-".concat(type_of_alert));
     $("#message_div").html(message).show("fast");
 
     timeout = setTimeout(function () {
-        $('#message_div').hide("slow"); // Hide the element after duration
+        timeout = null;
+        $('#message_div').hide("fast"); // Hide the element after duration
     }, duration);
 }
 
@@ -53,8 +56,6 @@ window.addEventListener("load", function () {
             Main();
         else
             Alert("No image has been selected","danger");
-        
-
     });
 
 
@@ -79,27 +80,34 @@ window.addEventListener("load", function () {
 
 
 
+// ====================== MAIN ===========================
 
-// TODO: Main
 async function Main(params?:any) {
     lib.assert(image_selected === true,"No image has been selected");
     
-    
-    // Prompt user to select any serial port.
-    const port = await navigator.serial.requestPort();
+    let port:any;
+
+    try {
+        // Prompt user to select any serial port.
+        port = await navigator.serial.requestPort();
+        device_selected = true;
+    } catch (error) {
+        Alert("Please select the device that you want \
+        to flash the firmware","danger");
+        return;
+    }
 
 
     let type_of_device = lib.GetTypeOfDevice(port);
     let device = lib.CreateInstanceOf[type_of_device](); // call dispatcher and create the isntance
     
-    device_selected = true;
 
 
 
 
 
 
-    // lib.assert(device_selected === true,"No image has been selected");
+    
 
 
     // TODO: FlashFirmware();
