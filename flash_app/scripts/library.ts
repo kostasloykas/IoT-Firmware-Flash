7da15a194d9985384cff30d49b4f53befcab0d0b
Kostas Loukas <kostas99loukas@gmail.com>
2023-05-23 12:09:32 +0300

    update app

--- flash_app/scripts/library.ts
+++ flash_app/scripts/library.ts
@@ -1,270 +1,268 @@
 /* library.ts */
 
 import { rejects } from "assert";
-import * as fs from "fs";
 import { resolve } from "path";
 
 declare global {
-    interface Navigator {
-        serial: any;
-    }
+  interface Navigator {
+    serial: any;
+  }
 }
 
 // ============================= VARIABLES =============================
 
-
-
-
 enum RESPOND {
-    COMMAND_RET_SUCCESS = 0x40,
-    COMMAND_RET_UNKNOWN_CMD = 0x41,
-    COMMAND_RET_INVALID_ADR = 0x43,
-    COMMAND_RET_FLASH_FAIL = 0x44
-};
+  COMMAND_RET_SUCCESS = 0x40,
+  COMMAND_RET_UNKNOWN_CMD = 0x41,
+  COMMAND_RET_INVALID_ADR = 0x43,
+  COMMAND_RET_FLASH_FAIL = 0x44,
+}
 
-
-const ACK = 0XCC;
+const ACK = 0xcc;
 const NACK = 0x33;
 
 export enum SUPPORTED_DEVICES {
-    CC2538 = "CC2538"
-};
+  CC2538 = "CC2538",
+}
 
 type DispatcherCreateInstance = {
-    [key :string]: () => CC2538;
+  [key: string]: () => CC2538;
 };
 
 // Dispatcher for creating instances auto of a specific device
-export const CreateInstanceOf:DispatcherCreateInstance = {
-    [SUPPORTED_DEVICES.CC2538] : () => {return new CC2538()}
-}
-
+export const CreateInstanceOf: DispatcherCreateInstance = {
+  [SUPPORTED_DEVICES.CC2538]: () => {
+    return new CC2538();
+  },
+};
 
 enum FILE_EXTENTION {
-    HEX = "hex"
-};
+  HEX = "hex",
+}
 
 enum VERSION_CC2538 {
-    _512_KB,
-    _256_KB,
-    _128_KB
-};
-
-
-
+  _512_KB,
+  _256_KB,
+  _128_KB,
+}
 
 // ============================= FUNCTIONS =============================
 
 export function PRINT(...anything: any): void {
-    console.log(anything);
+  console.log(anything);
 }
 
 export function ERROR(...anything: any): void {
-    throw new Error("ERROR: " + anything);
+  throw new Error("ERROR: " + anything);
 }
-
 
 export function DEBUG(...anything: any): void {
-    console.log("DEBUG", anything);
+  console.log("DEBUG", anything);
 }
 
-
 export function CheckForSerialNavigator(): void {
-    // Web Serial API is not available
-    if (!('serial' in navigator)) {
-        alert("Web Serial API is not available")
-        ERROR("Web Serial API is not available");
-    }
-
+  // Web Serial API is not available
+  if (!("serial" in navigator)) {
+    alert("Web Serial API is not available");
+    ERROR("Web Serial API is not available");
+  }
 }
 
 // Assertions to ensure that certain conditions or
 // assumptions hold true
 export function assert(condition: unknown, msg: string): asserts condition {
-    if (condition === false) throw new Error("Assertion: "+msg);
+  if (condition === false) throw new Error("Assertion: " + msg);
 }
-
-
 
 // ============================= INTERFACES =============================
 
-
-
 interface Command {
-    port:any;
-    writer:any;
-    reader:any;
-    Encoder:any;
-    Decoder:any;
-    baudrate:number;
+  port: any;
+  writer: any;
+  reader: any;
+  Encoder: any;
+  Decoder: any;
+  filters: object;
 
-
-
-    Open(...params: any): void;
-    Close(...params: any): void;
-    Write(...params: any): void;
-    Read(...params: any): void;
-    SendAck(...params: any): void;
-    SendNAck(...params: any): void;
-    ReceivePacket(...params: any): void;
-    SendSync(...params: any): void;
-    WaitForAck(...params: any): void;
-    CalculateCRC32(...params: any): void;
-    Download(...params: any): void;
-
-
+  OpenPort(...params: any): void;
+  ClosePort(...params: any): void;
+  MemoryWrite(...params: any): void;
+  MemoryRead(...params: any): void;
+  SendAck(...params: any): void;
+  SendNAck(...params: any): void;
+  ReceivePacket(...params: any): void;
+  SendSync(...params: any): void;
+  WaitForAck(...params: any): void;
+  CRC32(...params: any): void;
+  Download(...params: any): void;
+  Run(...params: any): void;
+  GetStatus(...params: any): void;
+  Ping(...params: any): void;
+  SendData(...params: any): void;
+  Reset(...params: any): void;
+  Erase(...params: any): void;
+  GetChipID(...params: any): void;
+  SetXOSC(...params: any): void;
 }
-
-
-
 
 // ============================= CLASSES =============================
 
-
 export class FirmwareFile {
+  private firmware_bytes: Uint8Array;
 
-    private firmware_bytes: Uint8Array;
+  public constructor(input_element: HTMLInputElement) {
+    this.CheckFileExtention(input_element.files[0].name);
+    this.ConvertFirmwareToBytes(input_element).then((bytes) => {
+      this.firmware_bytes = bytes;
+      DEBUG(bytes);
+    });
+  }
 
-    public constructor(input_element: HTMLInputElement) {
+  // Check file extention
+  private CheckFileExtention(name: string): void {
+    let extention: string = name.split(".").pop().toUpperCase();
+    DEBUG(name);
+    if (!(extention in FILE_EXTENTION)) ERROR("File extention is not supported");
+  }
 
-        this.CheckFileExtention(input_element.files[0].name);
-        this.ConvertFirmwareToBytes(input_element)
-            .then((bytes) =>{ this.firmware_bytes = bytes; DEBUG(bytes)});
-    }
+  // Convert firmware to bytes
+  private ConvertFirmwareToBytes(input_element: HTMLInputElement): Promise<Uint8Array> {
+    return new Promise<Uint8Array>((resolve, reject) => {
+      const reader = new FileReader();
 
-    // Check file extention
-    private CheckFileExtention(name: string): void {
-        let extention:string = name.split('.').pop().toUpperCase();
-        DEBUG(name);
-        if (!(extention in FILE_EXTENTION))
-            ERROR("File extention is not supported");
+      reader.onload = function (event) {
+        PRINT("File Uploaded");
+        const result = event.target?.result as ArrayBuffer;
+        const bytes = new Uint8Array(result);
+        resolve(bytes);
+      };
 
-    }
+      reader.onerror = function (event) {
+        reject(new Error("Error reading file."));
+      };
 
-    // Convert firmware to bytes
-    private ConvertFirmwareToBytes(input_element: HTMLInputElement): Promise<Uint8Array> {
-
-        return new Promise<Uint8Array>((resolve,reject)=>{
-            const reader = new FileReader();
-
-            reader.onload = function (event) {
-                PRINT("File Uploaded");
-                const result = event.target?.result as ArrayBuffer;
-                const bytes = new Uint8Array(result);
-                resolve(bytes);
-            };
-    
-            reader.onerror = function(event) {
-                reject(new Error('Error reading file.'));
-              };
-    
-            reader.readAsArrayBuffer(input_element.files[0]);
-
-        });
-    }
-          
-
+      reader.readAsArrayBuffer(input_element.files[0]);
+    });
+  }
 }
-
-
-
 
 export class Packet {
-    private size: number;
-    private checksum: number;
-    private data: Uint8Array;
+  private size: number;
+  private checksum: number;
+  private data: Uint8Array;
 
+  constructor(data: Uint8Array) {
+    this.data = data;
+    this.size = this.SizeOfPacket(data);
+    this.checksum = this.ComputeChecksum(data);
+  }
 
-    constructor(data: Uint8Array) {
-        this.data = data;
-        this.size = this.SizeOfPacket(data);
-        this.checksum = this.ComputeChecksum(data);
+  public SizeOfPacket(data: Uint8Array): number {
+    return 0;
+  }
 
-    }
-
-    public SizeOfPacket(data: Uint8Array): number {
-        return 0;
-    }
-
-    public ComputeChecksum(data: Uint8Array): number {
-        return 0;
-    }
-
-
-
-
+  public ComputeChecksum(data: Uint8Array): number {
+    return 0;
+  }
 }
-
-
-
-
 
 // ============================= SUPPORTED DEVICES =============================
 
-
 export class CC2538 implements Command {
-    private version: VERSION_CC2538;
-    port:any;
-    writer:any;
-    reader:any;
-    Encoder:any;
-    Decoder:any;
-    baudrate:number;
- 
-    // TODO: Flash firmware
-    FlashFirmware(port:any,image:FirmwareFile){
-        // Initialize
-        this.port = port;
-        this.reader = port.readable.getReader();
-        this.writer = port.writable.getWriter();
-        
-        this.Open(port)
+  private version: VERSION_CC2538;
+  port: any;
+  writer: any;
+  reader: any;
+  Encoder: any;
+  Decoder: any;
+  filters: object = {
+    dataBits: 8,
+    baudRate: 500000,
+    stopbits: 1,
+    parity: "none",
+    flowControl: "none", // Hardware flow control using the RTS and CTS signals is enabled.
+    bufferSize: 128000000,
+  };
 
+  // TODO: Flash firmware
+  FlashFirmware(port: any, image: FirmwareFile) {
+    // Initialize
+    this.port = port;
+    this.reader = port.readable.getReader();
+    this.writer = port.writable.getWriter();
 
-    }
+    this.OpenPort(); // Open port
 
+    this.ClosePort() // Close port
+      .then((port_closed) => {
+        if (port_closed) PRINT("Port closed");
+      })
+      .catch((err) => {
+        ERROR(err);
+      });
+  }
 
-
-    Open(port:any): void {
-        throw new Error("Method not implemented.");
-    }
-    Close(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    Write(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    Read(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    SendAck(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    SendNAck(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    ReceivePacket(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    SendSync(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    WaitForAck(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    CalculateCRC32(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-    Download(...params: any): void {
-        throw new Error("Method not implemented.");
-    }
-
-
-
-
+  OpenPort(): void {
+    this.port.open(this.filters);
+    PRINT("Port opened");
+  }
+  ClosePort(...params: any): Promise<boolean> {
+    return new Promise<boolean>(async (resolve, reject) => {
+      await this.reader.close();
+      await this.writer.close();
+      await this.port.close();
+      resolve(true);
+    });
+  }
+  MemoryWrite(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  MemoryRead(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  SendAck(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  SendNAck(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  ReceivePacket(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  SendSync(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  WaitForAck(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  CRC32(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  Download(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  Run(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  GetStatus(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  Ping(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  SendData(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  Reset(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  Erase(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  GetChipID(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
+  SetXOSC(...params: any): void {
+    throw new Error("Method not implemented.");
+  }
 }
-
-
-
-
-
--- flash_app/scripts/script.ts
+++ flash_app/scripts/script.ts
@@ -1,139 +1,116 @@
-
 /* script.ts */
 
-import * as lib from './library';
-import $, { error } from 'jquery';
-import { FirmwareFile } from './library';
+import * as lib from "./library";
+import $, { error } from "jquery";
+import { FirmwareFile } from "./library";
 
-let device_selected :boolean = false;
-let image_selected :boolean = false;
-let timeout:any = null;
-let image:FirmwareFile = null;
-let device_name:string = null;
-
-
+let device_selected: boolean = false;
+let image_selected: boolean = false;
+let timeout: any = null;
+let image: FirmwareFile = null;
+let device_name: string = null;
 
 // ====================== FUNCTIONS ==================
 
-function Alert(message:string,type_of_alert:string, duration:number = 4000) {
-    if (timeout !== null) {
-        clearTimeout(timeout);
-    }
-    
-    $("#message_div").removeClass(["alert-success","alert-danger"]);
-    $("#message_div").addClass("alert-".concat(type_of_alert));
-    $("#message_div").html(message).show("fast");
+function Alert(message: string, type_of_alert: string, duration: number = 4000) {
+  if (timeout !== null) {
+    clearTimeout(timeout);
+  }
 
-    timeout = setTimeout(function () {
-        timeout = null;
-        $('#message_div').hide("fast"); // Hide the element after duration
-    }, duration);
+  $("#message_div").removeClass(["alert-success", "alert-danger"]);
+  $("#message_div").addClass("alert-".concat(type_of_alert));
+  $("#message_div").html(message).show("fast");
+
+  timeout = setTimeout(function () {
+    timeout = null;
+    $("#message_div").hide("fast"); // Hide the element after duration
+  }, duration);
 }
-
-
-
-
-
-
 
 // ====================== ON LOAD OF PAGE ==================
 window.addEventListener("load", function () {
+  // Check if browser support Web Serial Api
+  lib.CheckForSerialNavigator();
 
-    // Check if browser support Web Serial Api
-    lib.CheckForSerialNavigator();
-    
-    
-    // ====================== EVENT LISTENERS ==================
+  // ====================== EVENT LISTENERS ==================
 
-    // event listener for flash firmware button
-    $("#flash_but").on("click", () => {
-        if(!device_selected){
-            Alert("No device selected","danger");
-            return;
-        }
-        if(!image_selected){
-            Alert("No image selected","danger");
-            return;
-        }
+  // event listener for flash firmware button
+  $("#flash_but").on("click", () => {
+    if (!device_selected) {
+      Alert("No device selected", "danger");
+      return;
+    }
+    if (!image_selected) {
+      Alert("No image selected", "danger");
+      return;
+    }
 
-        Main();
-    });
+    Main();
+  });
 
-    $("#device").on("change", () => {
-        device_name = $("#device").val() as string;
-        lib.PRINT("Device selected: ",device_name);
+  $("#device").on("change", () => {
+    device_name = $("#device").val() as string;
+    lib.PRINT("Device selected: ", device_name);
 
-        if(device_name == "null"){
-            Alert("No device selected","danger");
-            device_selected = false;
-            return;
-        }
+    if (device_name == "null") {
+      Alert("No device selected", "danger");
+      device_selected = false;
+      return;
+    }
 
-        Alert("Device selected " + device_name,"success");
-        device_selected = true;
-    });
+    Alert("Device selected " + device_name, "success");
+    device_selected = true;
+  });
 
+  // When image is being upload
+  $("#image").on("input", async () => {
+    let path: string = $("#image").val().toString();
+    if (path == "") {
+      image_selected = false;
+      Alert("Image unselected", "danger");
+      return;
+    }
+    const input_element = document.querySelector('input[type="file"]') as HTMLInputElement;
 
+    image = new FirmwareFile(input_element);
 
-    // When image is being upload  
-    $("#image").on("input", async () => {
-        let path: string = $("#image").val().toString();
-        if( path == ''){
-            image_selected = false;
-            Alert("Image unselected","danger");
-            return;
-        }
-        const input_element = document.querySelector('input[type="file"]') as HTMLInputElement;
+    Alert("Image uploaded successfully", "success");
+    image_selected = true;
+  });
 
-        image = new FirmwareFile(input_element);
+  // When device connect
+  navigator.serial.addEventListener("connect", (e: any) => {
+    Alert("Device connected", "primary");
+  });
 
-        Alert("Image uploaded successfully","success");
-        image_selected = true;
-
-    });
-
-
-    // When device connect
-    navigator.serial.addEventListener('connect', (e:any) => {
-        Alert("Device connected","primary");
-    });
-
-    // When device disconnect
-    navigator.serial.addEventListener('disconnect', (e:any)=> {
-        Alert("Device disconnected","danger");
-        lib.ERROR("Device disconnected");
-    });
-
-
+  // When device disconnect
+  navigator.serial.addEventListener("disconnect", (e: any) => {
+    Alert("Device disconnected", "danger");
+    lib.ERROR("Device disconnected");
+  });
 });
-
-
-
 
 // ====================== MAIN ===========================
 
 async function Main() {
-    lib.assert(image_selected === true,"No image has been selected");
-    lib.assert(device_selected === true,"No device has been selected");
-    lib.assert(device_name in lib.SUPPORTED_DEVICES ,"Device is not supported => " + device_name);
-    let port:any;
-    
-    try {
-        // Prompt user to select any serial port.
-        port = await navigator.serial.requestPort();
-    } catch (error) {
-        lib.PRINT(error);
-        Alert("Please select the device in order \
-        to flash the firmware","danger");
-        return;
-    }
+  lib.assert(image_selected === true, "No image has been selected");
+  lib.assert(device_selected === true, "No device has been selected");
+  lib.assert(device_name in lib.SUPPORTED_DEVICES, "Device is not supported => " + device_name);
+  let port: any;
 
+  try {
+    // Prompt user to select any serial port.
+    port = await navigator.serial.requestPort();
+  } catch (error) {
+    lib.ERROR(error);
+    Alert(
+      "Please select the device in order \
+        to flash the firmware",
+      "danger"
+    );
+    return;
+  }
 
-
-    let device = lib.CreateInstanceOf[device_name](); // call dispatcher and create the instance
-    device.FlashFirmware(port,image);
-
+  let device = lib.CreateInstanceOf[device_name](); // call dispatcher and create the instance
+  device.FlashFirmware(port, image);
 }
-
-
-
