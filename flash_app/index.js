a19941b072e98da1c94b5b71cba628e0ead8d85c
Kostas Loukas <kostas99loukas@gmail.com>
2023-07-20 11:01:31 +0300

    update app

--- flash_app/index.js
+++
@@ -1,9 +0,0 @@
-import { usb, getDeviceList } from "usb";
-
-const devices = getDeviceList();
-
-for (const device of devices) {
-    
-    if(device.deviceDescriptor.idVendor == 0x1915)
-        console.log(device);
-}
