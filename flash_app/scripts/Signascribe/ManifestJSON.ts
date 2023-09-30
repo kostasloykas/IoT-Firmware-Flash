560e9eb45121aac5dd27ab10d86d7aba37f75ce0
Kostas Loukas <kostas99loukas@gmail.com>
2023-09-30 23:37:13 +0300

    update app

--- flash_app/scripts/Signascribe/ManifestJSON.ts
+++ flash_app/scripts/Signascribe/ManifestJSON.ts
@@ -1,8 +1,8 @@
-import { DEBUG } from "../classes";
+import { DEBUG, ERROR, PRINT } from "../classes";
 
 export class ManifestJSON {
   // FIXME: create the attributes
-  private manifest_version: string = null;
+  private manifest_version: number = null;
   private common_name: number = null;
   private vendor_id: number = null;
   private product_id: number = null;
@@ -15,5 +15,29 @@ export class ManifestJSON {
 
   constructor(json: any) {
     // check the manifest version
+    this.manifest_version = Number.parseInt(json.manifest_version);
+
+    switch (this.manifest_version) {
+      case 1:
+        this.ManifestVersion_1(json);
+        break;
+
+      default:
+        ERROR("Unknown Manifest Version");
+        break;
+    }
+  }
+
+  // Manifest version is 1
+  private ManifestVersion_1(json: any) {
+    this.common_name = json.common_name;
+    this.vendor_id = json.vendor_id;
+    this.product_id = json.product_id;
+    this.timestamp = json.timestamp;
+    this.hash_algorithm = json.hash_algorithm;
+    this.firmware_size = json.firmware_size;
+    this.firmware_type = json.firmware_type;
+    this.sign_algorithm_public_key = json.sign_algorithm["public_key"];
+    this.sign_algorithm_type = json.sign_algorithm["type"];
   }
 }
