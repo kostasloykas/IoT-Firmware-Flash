43f9ef93dfa7df6c7138cf940c912eeab2c6e9c7
Kostas Loukas <kostas99loukas@gmail.com>
2023-10-05 14:09:12 +0300

    update app

--- flash_app/scripts/Signascribe/Certificate_chain.ts
+++ flash_app/scripts/Signascribe/Certificate_chain.ts
@@ -1,7 +1,7 @@
 import { Buffer } from "buffer";
 import { DEBUG, ERROR, assert } from "../classes";
-import * as https from "https";
 import axios from "axios";
+import { pki as x509 } from "node-forge";
 
 export class CertificateChain {
   private bytes: Buffer = null;
@@ -14,9 +14,11 @@ export class CertificateChain {
   constructor(bytes: Buffer) {
     this.bytes = bytes;
 
-    // init owner certificate and intermediate
+    // get owner certificate
 
-    // init common name
+    // get intermediate certificates
+
+    // get common name
 
     // FIXME: assertions
     // assert(this.owner_certificate != null, "");
@@ -28,21 +30,35 @@ export class CertificateChain {
     return null;
   }
 
-  // FIXME: Load CA's certificate list
-  private async LoadListOfTrustedCaCertificates(): Promise<string> {
+  // Load CA's certificate list
+  private async LoadListOfTrustedCaCertificates(): Promise<string[]> {
     // load mozilla's trusted certificates list
+    let mozillas_certificates: string = null;
+
     await axios
       .get("https://corsproxy.io/?https://curl.se/ca/cacert.pem")
       .then((response) => {
-        const responseData = response.data;
-        DEBUG(responseData);
+        mozillas_certificates = response.data;
       })
       .catch((err) => {
         ERROR(`Error fetching data:`, err);
       });
 
-    // make them all x509 objects
-    return null;
+    let certificates: string[] = this.SeparateX509Certificates(mozillas_certificates);
+
+    assert(mozillas_certificates != null, "Mozilla's certificates must be != null");
+    return certificates;
+  }
+
+  private SeparateX509Certificates(pemString: string): string[] {
+    const certificates: string[] = [];
+    const regex = /-----BEGIN CERTIFICATE-----[^-]*-----END CERTIFICATE-----/g;
+    let match;
+
+    while ((match = regex.exec(pemString)) !== null) {
+      certificates.push(match[0]);
+    }
+    return certificates;
   }
 
   // FIXME: GetOwnerCertificate
@@ -57,8 +73,30 @@ export class CertificateChain {
 
   // FIXME: Verify Certificate
   public async Verify() {
-    // load certificates
-    await this.LoadListOfTrustedCaCertificates();
+    // load trusted certificates
+    let ca_store: x509.CAStore = null;
+
+    await this.LoadListOfTrustedCaCertificates()
+      .then((mozilla_certificates) => {
+        ca_store = x509.createCaStore();
+
+        // some certificates can't include in the ca store
+        // i don't know why :)
+        for (let cert of mozilla_certificates) {
+          try {
+            ca_store.addCertificate(cert);
+          } catch (error) {}
+        }
+      })
+      .catch((err) => {
+        ERROR("LoadListOfTrustedCaCertificates", err);
+      });
+
+    assert(ca_store != null, "CA Store must be != null");
+
+    for (let cert of ca_store.listAllCertificates()) {
+      this.common_name = cert.issuer.attributes[1].value as string;
+    }
   }
 
   public get Bytes(): Buffer {
